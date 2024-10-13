0004f488465686e6b1857549a2c2176a77e94d2c
becrew <fiqru.s@gmail.com>
2024-10-13 21:15:57 +0700

    ll

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://165.154.208.232:3000/api',  // Pastikan ini adalah IP backend
-  socketUrl: 'http://165.154.208.232:3000'    // Ganti ini juga jika menggunakan WebSocket
+  apiUrl: 'http://labnav.my.id:3000/api',  // Pastikan ini adalah IP backend
+  socketUrl: 'http://labnav.my.id:3000'    // Ganti ini juga jika menggunakan WebSocket
 };
