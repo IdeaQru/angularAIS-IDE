4655cb454f22f6e623b8dbdfe7f54d40ee90f4dc
becrew <fiqru.s@gmail.com>
2024-10-28 11:07:01 +0700

    fix prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://svr.labnav.my.id:3000/api',  // Pastikan ini adalah IP backend
-  socketUrl: 'http://svr.labnav.my.id:3000'    // Ganti ini juga jika menggunakan WebSocket
+  apiUrl: 'http://labnav.my.id:3000/api',  // Pastikan ini adalah IP backend
+  socketUrl: 'http://labnav.my.id:3000'    // Ganti ini juga jika menggunakan WebSocket
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: true,
-  apiUrl: 'http://svr.labnav.my.id:3000/api',  // Pastikan ini adalah IP backend
-  socketUrl: 'http://svr.labnav.my.id:3000'    // Ganti ini juga jika menggunakan WebSocket
+  production: false,
+  apiUrl: 'http://localhost:3000/api',  // Pastikan ini adalah IP backend
+  socketUrl: 'http://localhost:3000'    // Ganti ini juga jika menggunakan WebSocket
 };
